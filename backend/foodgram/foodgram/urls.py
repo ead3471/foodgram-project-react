62abada3c4a2b1ed8421bda3c837d3d92f734c7c
ead3471 <ead3471@gmail.com>
2023-06-05 16:19:33 +0300

    remove external access to 8000 port

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,46 +1,51 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
-from api.views import (FoodgramUserViewSet,
-                       TagViewSet,
-                       IngredientViewSet,
-                       RecipeViewSet,
-                       FavoritesView,
-                       ShoppingCartView,
-                       SubscribeView,
-                       SubscriptionsView,
-                       ShoppingCartRenderView)
+from api.views import (
+    FoodgramUserViewSet,
+    TagViewSet,
+    IngredientViewSet,
+    RecipeViewSet,
+    FavoritesView,
+    ShoppingCartView,
+    SubscribeView,
+    SubscriptionsView,
+    ShoppingCartRenderView,
+)
 
 router_api = DefaultRouter()
-router_api.register(r'users', FoodgramUserViewSet, basename='user')
-router_api.register(r'tags', TagViewSet, basename='tags')
-router_api.register(r'ingredients', IngredientViewSet, basename='ingredients')
-router_api.register(r'recipes', RecipeViewSet)
+router_api.register(r"users", FoodgramUserViewSet, basename="user")
+router_api.register(r"tags", TagViewSet, basename="tags")
+router_api.register(r"ingredients", IngredientViewSet, basename="ingredients")
+router_api.register(r"recipes", RecipeViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('api/auth/', include('djoser.urls.authtoken')),
-
-    path(r'api/recipes/download_shopping_cart/',
-         ShoppingCartRenderView.as_view(), name='shopping_cart'),
-
-    path(r'api/users/subscriptions/',
-         SubscriptionsView.as_view(), name='subscriptions'),
-
-    path('api/', include(router_api.urls)),
-
-    path(r'api/recipes/<recipe_id>/favorite/',
-         FavoritesView.as_view(), name='favorite'),
-
-    path(r'api/recipes/<recipe_id>/shopping_cart/',
-         ShoppingCartView.as_view(), name='shopping_cart'),
-
-
-
-    path(r'api/users/<user_id>/subscribe/',
-         SubscribeView.as_view(), name='subscribe'),
-
-
-
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("djoser.urls.authtoken")),
+    path(
+        r"api/recipes/download_shopping_cart/",
+        ShoppingCartRenderView.as_view(),
+        name="shopping_cart",
+    ),
+    path(
+        r"api/users/subscriptions/",
+        SubscriptionsView.as_view(),
+        name="subscriptions",
+    ),
+    path("api/", include(router_api.urls)),
+    path(
+        r"api/recipes/<recipe_id>/favorite/",
+        FavoritesView.as_view(),
+        name="favorite",
+    ),
+    path(
+        r"api/recipes/<recipe_id>/shopping_cart/",
+        ShoppingCartView.as_view(),
+        name="shopping_cart",
+    ),
+    path(
+        r"api/users/<user_id>/subscribe/",
+        SubscribeView.as_view(),
+        name="subscribe",
+    ),
 ]
